2aeaca201e39fab20b67a6b6082fec29e958d31d
Igor Doronin <ihardaronin@gmail.com>
2024-07-02 10:46:33 +0300

    add dataframe

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
 import pandas as pd
 
-df = pd.read_csv('football_players_value_dataset.csv')
+data = {
+    'Name': ['Игорь', 'Олег', 'Андрей', 'Карина', 'Олеся', 'Тарас', 'Оксана', 'Арсен', 'Роберт', 'Камилла'],
+    'Mathematics': [9, 8, 9, 7, 6, 8, 9, 8, 9, 7],
+    'Physics': [9, 8, 7, 6, 6, 8, 9, 6, 8, 9],
+    'Russian': [8, 7, 8, 9, 6, 7, 8, 9, 8, 8],
+    'English': [9, 8, 10, 9, 8, 9, 10, 9, 7, 8],
+    'Biology': [7, 8, 9, 6, 7, 8, 9, 9, 8, 9]
+}
+
+df = pd.DataFrame(data)
 print(df.head())
-print(df.info())
-print(df.describe())
 
-dfs = pd.read_csv('dz.csv')
-print(dfs.head())
+print('Mathematics', df['Mathematics'].mean())
+print('Physics', df['Physics'].mean())
+print('Russian', df['Russian'].mean())
+print('English', df['English'].mean())
+print('Biology', df['Biology'].mean())
 
-group = dfs.groupby('City')['Salary'].mean()
-print(group)
+print('Mathematics', df['Mathematics'].median())
+print('Physics', df['Physics'].median())
+print('Russian', df['Russian'].median())
+print('English', df['English'].median())
+print('Biology', df['Biology'].median())
+
+print('Mathematics', df['Mathematics'].std())
+print('Physics', df['Physics'].std())
+print('Russian', df['Russian'].std())
+print('English', df['English'].std())
+print('Biology', df['Biology'].std())
+
+Q1_math = df['Mathematics'].quantile(0.25)
+Q3_math = df['Mathematics'].quantile(0.75)
+
+IQR = Q3_math - Q1_math
+print(IQR)
